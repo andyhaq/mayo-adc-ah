07c9be8121ddb3bcfc40687dee2bd651294bd0b7
andyhaq <59184552+andyhaq@users.noreply.github.com>
2026-05-31 21:28:50 -0500

    Update main.tf

--- modules/storage-bucket/main.tf
+++ modules/storage-bucket/main.tf
@@ -1,7 +1,6 @@
 resource "google_storage_bucket" "basic_bucket" {
-  name     = var.bucket_name
-  project  = var.project_id
-  location = var.region
-
+  name                        = var.bucket_name
+  project                     = var.project_id
+  location                    = var.region
   uniform_bucket_level_access = true
 }
