15544d4badaa4c6f5a0bc5fb14ccd65fe15462bb
andyhaq <59184552+andyhaq@users.noreply.github.com>
2026-05-31 21:29:08 -0500

    Update variables.tf

--- modules/storage-bucket/variables.tf
+++ modules/storage-bucket/variables.tf
@@ -1,14 +1,14 @@
 variable "bucket_name" {
   type        = string
-  description = "Globally unique name of the GCS bucket."
+  description = "Globally unique name for the bucket."
 }
 
 variable "project_id" {
   type        = string
-  description = "The GCP Project ID where the bucket will be created."
+  description = "Project where the bucket is created."
 }
 
 variable "region" {
   type        = string
-  description = "Bucket region (e.g., us-central1, us-west2, US)."
+  description = "Location for the bucket."
 }
